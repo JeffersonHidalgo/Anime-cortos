bf59f5cad3d81029db6c30f2570fe9f2258ddb63
samuelangeles <samueljoseangelereye@gmail.com>
2021-03-26 20:10:52 -0600

    trabajo de desarrollo de aplicacion

---
+++ paginaanime/no-game-no-life/inicio.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Peliculas</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="Shortcut Icon" type="image/x-icon" href="assets/icons/logoo.png" />
+    <script src="js/sweet-alert.min.js"></script>
+    <link rel="stylesheet" href="../../css/sweet-alert.css">
+    <link rel="stylesheet" href="../../css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="../../css/normalize.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/video.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="../../js/modernizr.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="../../js/main.js"></script>
+</head>
+<body background-color=" #ffdd90">
+    <div class="navbar-lateral full-reset">
+        <div class="visible-xs font-movile-menu mobile-menu-button"></div>
+        <div class="full-reset container-menu-movile custom-scroll-containers">
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+            <div class="full-reset" style="background-color:#2B3D51; padding: 10px 0; color:#fff;">
+                <figure>
+                    <img src="../../assets/img/logoo.png" alt="Biblioteca" class="img-responsive center-box" style="width:55%;">
+                </figure>
+                <p class="text-center" style="padding-top: 15px;"><font size="4" face="Algerian">the daily life of the immortal king</font></p>
+            </div>
+            <div class="full-reset nav-lateral-list-menu">
+                <ul class="list-unstyled">
+                    <li><a href="../../inicio.php"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
+                    <li>
+                        <div class="dropdown-menu-button"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp; Animes <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
+                        <ul class="list-unstyled">
+                        <li><a href="../demon-King-cademy/inicio.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Demon King academy</a></li>
+                            <li><a href="../mortal/inicio.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Mortal </a></li>
+                            <li><a href="../smarfon/inicio.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Isikai </a></li>
+                            <li><a href="../Horimiya/inicio.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Horimoya</a></li>
+                            <li><a href="esperamelodrama.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Melodrama</a></li>
+                            <li><a href="esperamusical.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Musical</a></li>
+                            <li><a href="esperaromance.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Romance</a></li>
+                            <li><a href="esperasuspense.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Suspense</a></li>
+                            <li><a href="esperaterror.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Terror </a></li>
+                        </ul>
+                    </li>
+                    	 <li><a href="espera.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Nuevas Peliculas</a></li>
+
+                    	 <li><a href="desconectar.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Cerrar</a></li>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content-page-container full-reset custom-scroll-containers">
+
+    	 <div class="footer-copyright full-reset all-tittles">Mundo anime</div>
+
+        <div class="container">
+            <div class="page-header">
+              <h1 class="all-tittles"><center><font size="10" face="Algerian">No game no life</font></center></h1>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <ul class="nav nav-tabs nav-justified"  style="font-size: 17px;">
+
+            </ul>
+        </div>
+        <div class="container-fluid"  style="margin: 50px 0;">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-3" style="width:30%; border-radius: 25px;">
+                    <img src="../../img/5.jpg" alt="user" class="img-responsive center-box" style="max-width: 300px;">
+                </div>
+                <div class="col-xs-12 col-sm-8 col-md-8 text-justify lead">
+                   <h2>sipnosis</h2>
+                    <p>Serie de TV (2014). 12 episodios. No Game, No Life se centra en Sora y Shiro, unos hermanos cuya reputación de NEETs hikikomoris y gamers han hecho que comiencen a extenderse leyendas urbanas suyas por todo internet.
+
+Estos dos gamers consideran que el mundo real no es más que “un juego de mierda”. Un día, un chico llamado “Dios” les invoca en un mundo alternativo. En este mundo, Dios ha prohibido la guerra y declarado que todo se decide en base a los juegos, incluso las fronteras de los países. La humanidad vive ahora en una ciudad por culpa del resto de razas. ¿Se convertirán Sora y Shiro, hermano y hermana, en los salvadores de este mundo alternativo? (FILMAFFINITY)
+<center>
+<h3>Título original</h3>
+<h3>No Game, No Life (TV Series)</h3>
+<h3>Año:2014</h3>
+<h3>Duración: 25 min.</h3>
+<h3>País: Japón Japón</h3>
+</center>
+
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+<div class="container-flat-form"style="
+width:80% ;
+    background: rgba(0,0,0,0.5);
+    color: white;
+    padding: 20px;
+    margin: 5%;
+    border-radius: 100px;
+    ">
+<center>
+<a href="1.php">
+<h1><img src="../../img/1.jpg" style=" width:90px;">__capitulo1__</h1>
+</a>
+</center>
+<br>
+<center>
+<a href="2.php">
+<h1><img src="../../img/1.jpg" style=" width:90px;">__capitulo 2__</h1>
+</a>
+</center>
+
+<br>
+<center>
+<a href="3.php">
+<h1>capitulo 3</h1>
+</a>
+</center>
+<br>
+<center>
+<a href="4.php">
+<h1>capitulo 4</h1>
+</a>
+</center>
+<br>
+<center>
+<a href="5.php">
+<h1>capitulo 5</h1>
+</a>
+</center>
+<br>
+<center>
+<a href="6.php">
+<h1>capitulo 6</h1>
+</a>
+</center>
+<br>
+<center>
+<a href="7.php">
+<h1>capitulo 7</h1>
+</a>
+</center>
+<br>
+<center>
+<a href="8.php">
+<h1>capitulo 8</h1>
+</a>
+</center>
+<br>
+<center>
+<a href="9.php">
+<h1>capitulo 9</h1>
+</a>
+</center>
+<br>
+<center>
+<a href="10.php">
+<h1>capitulo 10</h1>
+</a>
+</center>
+<br>
+<center>
+<a href="11.php">
+<h1>capitulo 11</h1>
+</a>
+</center>
+<br>
+<center>
+<a href="12.php">
+<h1>capitulo 12</h1>
+</a>
+</center>
+<br>
+<center>
+<a href="13.php">
+<h1>capitulo 13</h1>
+</a>
+</center>
+</div>
+
+
+
+        </div>
+        <footer class="footer full-reset">
+
+            <div class="footer-copyright full-reset all-tittles">Mundo anime</div>
+        </footer>
+    </div>
+</body>
+</html>
